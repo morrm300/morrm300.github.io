44ec525b820ead8a27446181308e2501949c7783
mintra <morrm300@newschool.edu>
2016-03-30 23:04:42 -0400

    msg ...

---
+++ quiz-3/assets/js/app.js
@@ -0,0 +1,18 @@
+var color = "blue";
+var wallet = 1200;
+var converted_wallet = 0;
+var conversion_rate = [0.88, 112.34, 0.70];
+
+
+var EUR = 0.88;
+var YEN = 112.34;
+var GDP = 0.70;
+
+function convert_currency() {
+	var converted_wallet
+
+	if (EUR == 929.28) {
+
+
+ });
+
